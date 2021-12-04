b5f3c4a0091a448a9f296a1cb35d1b350e366ea2
Hugo Müller-Downing <github@hu.md>
2021-12-04 13:44:05 +1100

    Day 1, part 2

--- 01/index.js
+++ 01/index.js
@@ -8,25 +8,33 @@ const countIncreases = (input) => {
   let pointer = 0;
 
   while (pointer < input.length - 1) {
-    if (pointer == 0) {
-      console.log(input[pointer] + ' (N/A, no previous measurement)')
-    }
-
     if (input[pointer + 1] > input[pointer]) {
       counter += 1;
-      console.log(input[pointer + 1] + ' (increased)')
-    } else if (input[pointer + 1] > input[pointer]){
-      console.log(input[pointer + 1] + ' (decreased)')
     }
 
     pointer += 1;
   }
 
-  console.log(counter);
+  return counter;
+};
+
+const groupThrees = (input) => {
+  let pointer = 0;
+
+  const groups = [];
+
+  while (pointer + 2 <= input.length - 1) {
+    const group = [input[pointer], input[pointer + 1], input[pointer + 2]]
+    groups.push(group.reduce((acc, val) => acc + val, 0));
+
+    pointer += 1;
+  }
+
+  return groups;
 };
 
 fs.readFile('input', (err, data) => {
   if (err) throw err;
   const lines = data.toString().split('\n').map(i => parseInt(i));
-  countIncreases(lines);
+  console.log(countIncreases(groupThrees(lines)));
 });
